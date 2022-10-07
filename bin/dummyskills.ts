eb908914a6357eb588e0654cf555732ed3e8e57d
skandog <muumrar@gmail.com>
2022-10-07 16:59:01 +0100

    added another skill to the list

--- bin/dummyskills.ts
+++ bin/dummyskills.ts
@@ -8,4 +8,5 @@ export type Skill = {
   export const dummySkills: Skill[] = [
     {id: 1, name: 'Trail Blazer', count: 2, type: 'Soft Skills'},
     {id: 2, name: 'Typescript', count: 3, type: 'dev'},
+    {id: 3, name: 'Java', count: 0, type: 'dev'},
   ];
